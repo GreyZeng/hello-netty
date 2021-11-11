0c883aa3d0a7db2fb11cd8a58a8b746bbd26c817
zenghui <410486047@qq.com>
2021-11-11 09:51:49 +0800

    netty

---
+++ src/main/java/git/snippets/netty/v6/TimeDecoder.java
@@ -0,0 +1,22 @@
+package git.snippets.netty.v6;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.ByteToMessageDecoder;
+
+import java.util.List;
+
+/**
+ * @author <a href="mailto:410486047@qq.com">Grey</a>
+ * @date 2021/11/11
+ * @since
+ */
+public class TimeDecoder extends ByteToMessageDecoder { // (1)
+    @Override
+    protected void decode(ChannelHandlerContext ctx, ByteBuf in, List<Object> out) { // (2)
+        if (in.readableBytes() < 4) {
+            return; // (3)
+        }
+        out.add(in.readBytes(4)); // (4)
+    }
+}
---
+++ src/main/java/git/snippets/netty/v7/TimeServerHandler.java
@@ -0,0 +1,33 @@
+package git.snippets.netty.v7;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelFutureListener;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+/**
+ * @author <a href="mailto:410486047@qq.com">Grey</a>
+ * @date 2021/11/11
+ * @since
+ */
+public class TimeServerHandler extends ChannelInboundHandlerAdapter {
+    // the channelActive() method will be invoked when a connection is established and ready to generate traffic.
+    @Override
+    public void channelActive(final ChannelHandlerContext ctx) { // (1)
+        final ByteBuf time = ctx.alloc().buffer(4); // (2)
+        time.writeInt((int) (System.currentTimeMillis() / 1000L + 2208988800L));
+
+        final ChannelFuture f = ctx.writeAndFlush(time); // (3)
+        f.addListener((ChannelFutureListener) future -> {
+            assert f == future;
+            ctx.close();
+        }); // (4)
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
