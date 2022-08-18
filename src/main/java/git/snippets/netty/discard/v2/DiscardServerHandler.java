7e785389001fe5f3559db4fb1745d7f260c06579
zenghui <410486047@qq.com>
2022-08-18 16:29:13 +0800

    two ways to read bytes

--- src/main/java/git/snippets/netty/discard/v2/DiscardServerHandler.java
+++ src/main/java/git/snippets/netty/discard/v2/DiscardServerHandler.java
@@ -3,6 +3,7 @@ package git.snippets.netty.discard.v2;
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.CharsetUtil;
 import io.netty.util.ReferenceCountUtil;
 
 /**
@@ -13,10 +14,14 @@ public class DiscardServerHandler extends ChannelInboundHandlerAdapter {
     public void channelRead(ChannelHandlerContext ctx, Object msg) { // (2)
         ByteBuf in = (ByteBuf) msg;
         try {
-            while (in.isReadable()) { // (1)
-                System.out.print((char) in.readByte());
-                System.out.flush();
-            }
+            // 两种写法
+            // 第一种写法
+//            while (in.isReadable()) { // (1)
+//                System.out.print((char) in.readByte());
+//                System.out.flush();
+//            }
+            // 第二种写法
+            System.out.print(in.toString(CharsetUtil.UTF_8));
         } finally {
             ReferenceCountUtil.release(msg); // (2)
         }
