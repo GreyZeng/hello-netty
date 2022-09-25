9062c1a877d68305227397e4dd23e5a7eada049d
GreyZeng <410486047@qq.com>
2022-09-25 16:14:20 +0800

    remove useless package

--- src/main/java/snippet/chat/server/NettyServer.java
+++ src/main/java/snippet/chat/server/NettyServer.java
@@ -1,14 +1,12 @@
 package snippet.chat.server;
 
-import snippet.chat.protocol.PacketDecoder;
-import snippet.chat.protocol.PacketEncoder;
-import snippet.chat.protocol.Splitter;
 import io.netty.bootstrap.ServerBootstrap;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.ChannelOption;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
+import snippet.chat.protocol.Splitter;
 
 import java.util.Date;
 
