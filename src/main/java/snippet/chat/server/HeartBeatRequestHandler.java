5208c0da79a6e5b139799f4f62664b39238dd9bc
GreyZeng <410486047@qq.com>
2022-09-26 13:31:08 +0800

    update

---
+++ src/main/java/snippet/chat/server/HeartBeatRequestHandler.java
@@ -0,0 +1,9 @@
+package snippet.chat.server;
+
+/**
+ * @author <a href="mailto:410486047@qq.com">Grey</a>
+ * @date 2022/9/21
+ * @since
+ */
+public class HeartBeatRequestHandler {
+}
---
+++ src/main/java/snippet/chat/server/IMIdleStateHandler.java
@@ -0,0 +1,12 @@
+package snippet.chat.server;
+
+import io.netty.channel.ChannelHandler;
+import io.netty.util.concurrent.EventExecutorGroup;
+
+/**
+ * @author <a href="mailto:410486047@qq.com">Grey</a>
+ * @date 2022/9/21
+ * @since
+ */
+public class IMIdleStateHandler implements ChannelHandler, EventExecutorGroup {
+}
