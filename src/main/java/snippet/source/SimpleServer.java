834635e4623b944ea6c079cce292c8cbee0a0d23
GreyZeng <410486047@qq.com>
2022-09-26 16:53:12 +0800

    update

--- src/main/java/snippet/source/SimpleServer.java
+++ src/main/java/snippet/source/SimpleServer.java
@@ -19,21 +19,31 @@ import io.netty.channel.socket.nio.NioServerSocketChannel;
  */
 public final class SimpleServer {
     public static void main(String[] args) throws InterruptedException {
+        // EventLoopGroup: 服务端的线程模型外观类。这个线程要做的事情
+        // 就是不停地检测IO事件，处理IO事件，执行任务。
         EventLoopGroup bossGroup = new NioEventLoopGroup();
         EventLoopGroup workerGroup = new NioEventLoopGroup();
         try {
+            // 服务端的一个启动辅助类。通过给它设置一系列参数来绑定端口启动服务。
             ServerBootstrap b = new ServerBootstrap();
             b
+                    // 设置服务端的线程模型。
+                    // bossGroup 负责不断接收新的连接，将新的连接交给 workerGroup 来处理。
                     .group(bossGroup, workerGroup)
+                    // 设置服务端的 IO 类型是 NIO。Netty 通过指定 Channel 的类型来指定 IO 类型。
                     .channel(NioServerSocketChannel.class)
+                    // 服务端启动过程中，需要经过哪些流程。
                     .handler(new SimpleServerHandler())
+                    // 用于设置一系列 Handler 来处理每个连接的数据
                     .childHandler(new ChannelInitializer<SocketChannel>() {
                         @Override
                         protected void initChannel(SocketChannel socketChannel) {
 
                         }
                     });
+            // 绑定端口同步等待。等服务端启动完毕，才会进入下一行代码
             ChannelFuture f = b.bind(8888).sync();
+            // 等待服务端关闭端口绑定，这里的作用是让程序不会退出
             f.channel().closeFuture().sync();
         } finally {
             bossGroup.shutdownGracefully();
